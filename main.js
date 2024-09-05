3f036309f3f136a8ef3b9bfb7501c6201b060749
Michael Gauthier <mikegauthier222@hotmail.com>
2024-09-05 13:41:27 -0700

    add scroll 3d

--- main.js
+++ main.js
@@ -27,19 +27,64 @@ const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
-const pointLight = new THREE.PointLight(0xFFFFFF, 10000);
+const pointLight = new THREE.PointLight(0xffffff, 100);
 pointLight.position.set(5, 5, 5);
 
-const ambientLight = new THREE.AmbientLight(0xFFFFFF);
-scene.add(pointLight, ambientLight)
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50)
-scene.add(lightHelper, gridHelper)
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);
 
 scene.add(pointLight);
 
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const forestTexture = new THREE.TextureLoader().load("forest.jpg");
+scene.background = forestTexture;
+
+const mikeTexture = new THREE.TextureLoader().load("me.jpg");
+
+const mike = new THREE.Mesh(
+  new THREE.BoxGeometry(5, 5, 5),
+  new THREE.MeshBasicMaterial({ map: mikeTexture })
+);
+
+const ballTexture = new THREE.TextureLoader().load('painted_brick.jpg');
+
+const ball = new THREE.Mesh(
+  new THREE.SphereGeometry( 3, 32, 32),
+  new THREE.MeshStandardMaterial( {
+    map: ballTexture
+  })
+)
+
+scene.add(mike)
+scene.add(ball)
+
+ball.position.z = 30
+ball.position.setX(-10);
+
+mike.position.z = 20
+
+function moveCamera() {
+
+  const t = document.body.getBoundingClientRect().top;
+  ball.rotation.x += 0.05;
+  ball.rotation.y += 0.075;
+  ball.rotation.z += 0.05;
+
+  mike.rotation.y += 0.01;
+  mike.rotation.z += 0.01;
+
+  camera.position.z = 20 + t * -0.05;
+  camera.position.x = t * -0.002;
+  camera.position.y = t * -0.002;
+
+}
+
+document.body.onscroll = moveCamera
 
 function animate() {
   requestAnimationFrame(animate);
